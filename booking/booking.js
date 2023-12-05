964bf8155bfd8feab21fd0cc8a27b923ab77aaf5
Anna Ngan <anna.ymngan@gmail.com>
2023-12-05 23:12:50 +0800

    Add Booking Success HTML

--- booking/booking.js
+++ booking/booking.js
@@ -1,38 +1,35 @@
-const timePicker = document.querySelector(".time-picker")
-const timeDisplay = document.querySelector(".time")
-const dateDisplay = document.querySelector(".date")
+const timePicker = document.querySelector(".time-picker");
+const timeDisplay = document.querySelector(".time");
+const dateDisplay = document.querySelector(".date");
 
+function loadTimePicker(start_time, end_time, interval) {
+  timePicker.innerHTML = "";
+  for (let i = start_time; i < end_time; i += interval) {
+    timePicker.innerHTML += `<div class="timeslot time-${i}">${i}:00 - ${
+      i + 1
+    }:00</div>`;
+  }
 
-function loadTimePicker (start_time, end_time,interval){
+  const timeslots = document.querySelectorAll(".timeslot");
+  const displaySelectedTime = document.querySelectorAll(
+    ".display-selected-time"
+  );
 
-  timePicker.innerHTML = ""
-    for (let i = start_time;i < end_time; i+=interval){
-        timePicker.innerHTML += `<div class="timeslot time-${i}">${i}:00 - ${i+1}:00</div>`
-    }
+  timeslots.forEach((timeslot, index) => {
+    timeslot.addEventListener("click", (e) => {
+      let timeslotId = e.target.classList[1].slice(5);
 
-    const timeslots = document.querySelectorAll('.timeslot') 
-    const displaySelectedTime = document.querySelectorAll('.display-selected-time')
-
-    timeslots.forEach((timeslot,index) => {
-        timeslot.addEventListener('click', (e) => {
-            let timeslotId = e.target.classList[1].slice(5)
-
-            //Unselect the selected timeslot
-            if(e.target.classList.contains("time-selected")){
-              e.target.classList.remove("time-selected")
-              //remove the unselect timeslot from booking time line
-              document.querySelector(`span.time-${timeslotId}`).remove()
-            }else{
-              e.target.classList.add("time-selected")
-              timeDisplay.innerHTML += `<span class="display-selected-time time-${timeslotId}"> ${e.target.innerText}</span>`
-            }
-           
-        })
-
-    })
-
-    
-   
+      //Unselect the selected timeslot
+      if (e.target.classList.contains("time-selected")) {
+        e.target.classList.remove("time-selected");
+        //remove the unselect timeslot from booking time line
+        document.querySelector(`span.time-${timeslotId}`).remove();
+      } else {
+        e.target.classList.add("time-selected");
+        timeDisplay.innerHTML += `<span class="display-selected-time time-${timeslotId}"> ${e.target.innerText}</span>`;
+      }
+    });
+  });
 }
 
 function addDays(date, days) {
@@ -40,83 +37,51 @@ function addDays(date, days) {
   return date;
 }
 
-
 //Fullcalendar Configuration
-var calendarEl = document.getElementById('calendar');
+var calendarEl = document.getElementById("calendar");
 var calendar = new FullCalendar.Calendar(calendarEl, {
-    // initialView: 'dayGridMonth',
-    initialView: 'multiMonthFourMonth',
-    views: {
+  // initialView: 'dayGridMonth',
+  initialView: "multiMonthFourMonth",
+  views: {
     multiMonthFourMonth: {
-      type: 'multiMonth',
-      duration: { months: 2 }
-    }},
-    validRange: {
-      start: addDays(new Date(), 2),
-      end: addDays(new Date(), 32)
+      type: "multiMonth",
+      duration: { months: 2 },
     },
-    headerToolbar:false
-    
-  });
-  
+  },
+  validRange: {
+    start: addDays(new Date(), 2),
+    end: addDays(new Date(), 32),
+  },
+  headerToolbar: false,
+});
+
 calendar.render();
 
 //Display Time when date on calendar is clicked
-const days = document.querySelectorAll('td[data-date]')
+const days = document.querySelectorAll("td[data-date]");
 
-days.forEach((day,index) => {
-  day.addEventListener('click', (e)=>{
-    if(!e.target.closest('td[data-date]').classList.contains("fc-day-other")){
-      let selectedDay = e.target.closest('td[data-date]').getAttribute("data-date")
-      dateDisplay.innerText = `Booking Date: ${selectedDay}`
-      timeDisplay.innerText = "Booking Time:"
-      setDayActive(selectedDay)
-      loadTimePicker(20,24,1)
+days.forEach((day, index) => {
+  day.addEventListener("click", (e) => {
+    if (!e.target.closest("td[data-date]").classList.contains("fc-day-other")) {
+      let selectedDay = e.target
+        .closest("td[data-date]")
+        .getAttribute("data-date");
+      dateDisplay.innerText = `Booking Date: ${selectedDay}`;
+      timeDisplay.innerText = "Booking Time:";
+      setDayActive(selectedDay);
+      loadTimePicker(20, 24, 1);
     }
-  })
-})
+  });
+});
 
 function setDayActive(selectedDay) {
   days.forEach((day) => {
-    if(day.getAttribute("data-date") === selectedDay ) { 
-      if(!day.classList.contains("fc-day-other")){
-        day.classList.add("date-selected"); 
+    if (day.getAttribute("data-date") === selectedDay) {
+      if (!day.classList.contains("fc-day-other")) {
+        day.classList.add("date-selected");
       }
-    }else { 
-      day.classList.remove("date-selected"); }
+    } else {
+      day.classList.remove("date-selected");
+    }
   });
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
