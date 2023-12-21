8af37cb81f87d77276e573b5bc812290f435d136
Brian Chan <ccbwork2019@gmail.com>
2023-12-22 01:05:33 +0800

    solved login bug when typing wrong email

--- routes/loginRoutes.ts
+++ routes/loginRoutes.ts
@@ -13,25 +13,29 @@ async function ownerLogin(req:Request, res:Response){
     console.log(req.body)
 
 	const result = await client.query(`SELECT email, password FROM owner where email = $1`, [req.body.email])
-    
+
 	const [owner]: Owner[] = result.rows
 
-	const matched = await checkPassword(req.body.password, owner.password)
-   
-	if (matched) {
-		console.log('login success')
-		req.session.owner = req.body.email
+    if (owner){
+        const matched = await checkPassword(req.body.password, owner.password)
         
-        //Get owner's id from owner table and put in session cookie
-        const ownerIDResult = await client.query(`SELECT id FROM owner WHERE email = $1`,[req.body.email])
-        const ownerID = ownerIDResult.rows[0].id
-        req.session.owner_id = ownerID
-
-        //res.redirect('/admin.html')
-        
-        res.status(200).json({message:"success"})
-	}else{
-        // res.redirect('/login/login.html?error=login-failed')
+        if (matched) {
+            console.log('login success')
+            req.session.owner = req.body.email
+            
+            //Get owner's id from owner table and put in session cookie
+            const ownerIDResult = await client.query(`SELECT id FROM owner WHERE email = $1`,[req.body.email])
+            const ownerID = ownerIDResult.rows[0].id
+            req.session.owner_id = ownerID
+    
+            //res.redirect('/admin.html')
+            
+            res.status(200).json({message:"success"})
+        }else{
+            // res.redirect('/login/login.html?error=login-failed')
+            res.status(401).json({message:"error"})
+        }
+    } else {
         res.status(401).json({message:"error"})
     }
 
